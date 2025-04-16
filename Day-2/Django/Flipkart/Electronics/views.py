5db46c8efcc4b9d5176a433ed22a8637f3e75ed9
PriyanshuKSharma <priyanshu17ks@gmail.com>
2025-04-16 17:11:23 +0530

    Django project

--- Day-2/Django/Flipkart/Electronics/views.py
+++ Day-2/Django/Flipkart/Electronics/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def home(request):
+    prod=Product.objects.all()
+    context = {'products': prod}
+    return render(request, 'Electronics/home.html', {'prod': prod})
