f8711c2ffd253e45c236efe7ae2a5638851eec55
Priyanshu K Sharma <153804042+itspriyanshuks17@users.noreply.github.com>
2025-04-17 16:17:16 +0530

    To accept form from front-end

--- Day-2/Django/Flipkart/Electronics/views.py
+++ Day-2/Django/Flipkart/Electronics/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
-from .models import Product
-from .forms import ProductForm
-
+from math import prod
+from django.shortcuts import render  # type: ignore
+from .models import Product # Import the Product model
+from .forms import ProductForms # Import the ProductForms class
+from django.shortcuts import redirect # type: ignore
+# Create your views here.
 def home(request):
-    prod=Product.objects.all() # Fetch all products from the database
-    context = {'products': prod} # Create a context dictionary to pass to the template
-    fm = ProductForm() # Create an instance of the ProductForm
-    return render(request, 'Electronics/home.html', {'prod': prod, 'fm':fm})
+    prod = Product.objects.all() #fetching all the objects from the Product model
+    if request.method == 'POST':
+        fm = ProductForms(request.POST)
+        if fm.is_valid():
+            fm.save()
+            return redirect ("Success")
+    else:
+        fm = ProductForms()
+    return render(request, 'Electronics/home.html', {'prod' : prod, 'fm': fm}) #rendering the home.html template in the Electronics directory
+    
+    prod = Product.objects.all()
+    fm = ProductForms() #fetching all the objects from the Product model
+    return render(request, 'Electronics/home.html', {'prod':prod, 'fm': fm}) #rendering the home.html template in the Electronics directory
